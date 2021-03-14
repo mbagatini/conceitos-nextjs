e86fc8280456047c5007326c97a118c92f127af8
Morgana Bagatini <morganabagatini@gmail.com>
2021-03-14 09:20:07 -0300

    Client side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from 'react';
+
 import { Title } from '../styles/pages/Index';
 
+interface IProduct {
+	id: number;
+	title: string;
+}
+
 export default function Home() {
+	const [recomended, setRecomended] = useState<IProduct[]>([]);
+
+	useEffect(() => {
+		// Client side rendering
+		fetch('http://localhost:3030/recommended').then(response => {
+			response.json().then(data => {
+				setRecomended(data);
+			})
+		})
+	}, []);
+
 	return (
 		<div>
-			<Title>Hello Next JS! 🤩</Title>
+			<Title>Products</Title>
+			<ul>
+				{recomended.map(product => {
+					return (
+						<li key={product.id}>
+							{product.title}
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	)
 }
